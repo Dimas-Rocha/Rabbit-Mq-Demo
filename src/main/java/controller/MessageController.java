9f4477d0608e767ded291438fe26985fbf4d9107
Dimas Rocha <dimas.dasp@gmail.com>
2026-01-15 21:36:07 -0300

    RabbitMQ Controller

---
+++ src/main/java/controller/MessageController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import dto.MessageDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import producer.RabbitMQProducer;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/message")
+public class MessageController {
+    private final RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    public ResponseEntity<String> sendMessage(@RequestBody MessageRequest request){
+       MessageDTO messageDTO = new MessageDTO(
+               UUID.randomUUID().toString(),
+               request.getMessage(),
+               LocalDateTime.now(),
+                       "API"
+       );
+       producer.sendMessageI(messageDTO);
+       return ResponseEntity.ok("Mensagem enviada para filial!");
+    }
+
+    public static class MesssageRequest{
+        private String message;
+
+        public String getMessage() {
+            return message;
+        }
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+
+}
--- src/main/java/producer/RabbitMQProducer.java
+++ src/main/java/producer/RabbitMQProducer.java
@@ -1,8 +1,8 @@
 package producer;
 
-import
+
 import dto.MessageDTO;
-import org.slf4j.Logger
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
